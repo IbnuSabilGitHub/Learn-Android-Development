72e2826a55faf73ee74b20047e34bdf25004cf97
IbnuSabil <ibnusabil2301@gmail.com>
2023-10-29 13:04:55 +0700

    Type Data Number

---
+++ Learn/Kotlin/1-TypeDataNumber.kt
@@ -0,0 +1,25 @@
+fun main (){
+//    Type Data Bilangan real
+    var byteData: Byte = 23
+    var Integer: Int = 100
+    var shortData : Short = 2000
+    var longData : Long = 300000L
+    println(byteData)
+    println(Integer)
+    println(shortData)
+    println(longData)
+
+//    Type Data bilangan pecahan
+    var floatData: Float = 10.23F
+    var doubleData : Double = 30.2827
+    println(floatData)
+    println(doubleData)
+
+//    Konfersi type data
+    var number: Double = 29.330
+    var numberInt: Int = number.toInt()
+    print(numberInt)
+
+
+
+}
