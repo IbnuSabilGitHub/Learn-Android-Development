5ca31bec2036a02b91827931ca6efce8aae139a6
IbnuSabil <ibnusabil2301@gmail.com>
2023-11-04 19:45:42 +0800

    Menambahkan Single Expression Function

--- Learn/Kotlin/11-FunctionReturnType.kt
+++ Learn/Kotlin/11-FunctionReturnType.kt
@@ -4,7 +4,11 @@ fun quadrat(x:Int):Int{
    val quad = x * x
     return quad
 }
+//Single Expression Function
+fun quadratSingle(x:Int):Int = x * x
 fun main() {
-    val result = quadrat(2)
+    val result = quadrat(5)
+    val resultSingle = quadratSingle(10)
     println(result)
+    println(resultSingle)
 }
