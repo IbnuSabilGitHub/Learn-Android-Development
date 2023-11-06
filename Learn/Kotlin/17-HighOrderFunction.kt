56c1396f465c631dd732c5bba20a507282b72ff5
IbnuSabil <ibnusabil2301@gmail.com>
2023-11-06 19:44:21 +0800

    High Oreder Function

---
+++ Learn/Kotlin/17-HighOrderFunction.kt
@@ -0,0 +1,22 @@
+tailrec fun factorial(n: Int, buffer: Int = 1):Int{
+    return when(n){
+        1 -> buffer
+        else -> factorial(n - 1, n * buffer)
+    }
+}
+fun Int.quad(n:Int):Int = this * this
+
+fun main() {
+    fun math(num: Int, transform: (Int) -> Int):Int = transform(num)
+
+
+    println(math(2,{num:Int -> num.quad()}))
+
+//    Trailling Lambda
+    val result = math(2){num ->
+        num.quad()
+    }
+    println(result)
+    println(math(5,{num:Int -> factorial(num)}))
+}
+
