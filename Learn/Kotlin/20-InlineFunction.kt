cf12a90bb7abd8ef33d6cc40d73abb4b1f6a097f
IbnuSabil <ibnusabil2301@gmail.com>
2023-11-10 21:43:04 +0800

    Inline FUnction

---
+++ Learn/Kotlin/20-InlineFunction.kt
@@ -0,0 +1,20 @@
+
+
+inline fun math(num: Int, oprations: (Int) -> Int):Int = oprations(num)
+inline fun char(value: String, oprations: (String) -> String):String = oprations(value)
+
+fun main() {
+    fun Int.quad():Int = this * this
+    fun String.fungsi():Unit = println("Hello ${this}")
+
+
+    val result = math(5){num:Int ->
+        num.quad()
+    }
+
+    val resultString = char("kotline"){num:String ->
+        num.uppercase()
+    }
+
+    println(result)
+}
