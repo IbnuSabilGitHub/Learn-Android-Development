20db147279ec639c407484b5730e0fa8a1fa689a
IbnuSabil <ibnusabil2301@gmail.com>
2023-11-24 20:03:12 +0800

    Secondary Constructor

--- Constructor.kt
+++ Constructor.kt
@@ -1,17 +1,29 @@
 import data.Laptop
 
 fun main() {
-    var laptop = Laptop(
+    var LaptopHp = Laptop(
         "Hp",
         "VICTUS GAMING",
-        2024
+        
     )
-    laptop.NoUnit = 654
-    laptop.NoProduksi = 958
-    println(laptop.brand)
-    println(laptop.type)
-    println(laptop.NoUnit)
-    println(laptop.NoProduksi)
-    println(laptop.TahunProduksi)
+
+    LaptopHp.NoUnit = 654
+    LaptopHp.NoProduksi = 958
+    println(LaptopHp.brand)
+    println(LaptopHp.type)
+    println(LaptopHp.NoUnit)
+    println(LaptopHp.NoProduksi)
+    println(LaptopHp.TahunProduksi)
+
+
+    var LaptopAsus = Laptop("Asus")
+
+    LaptopAsus.NoUnit = 654
+    LaptopAsus.NoProduksi = 958
+    println(LaptopAsus.brand)
+    println(LaptopAsus.type)
+    println(LaptopAsus.NoUnit)
+    println(LaptopAsus.NoProduksi)
+    println(LaptopAsus.TahunProduksi)
 }
 
--- Learn/Kotlin-OOP/classAndObject/data/Laptop.kt
+++ Learn/Kotlin-OOP/classAndObject/data/Laptop.kt
@@ -1,9 +1,19 @@
 package data
 
+import kotlin.contracts.contract
+
 class Laptop (Brand:String, Type:String , Year:Int){
     init {
         println("Brand $Brand dengan type $Type Pada Tahun $Year")
     }
+    constructor(Brand:String, Type:String) : this(Brand, Type, 2014){
+        println("Constructor 1 di buat")
+        }
+    constructor(Brand:String) : this(Brand, "Unknown"){
+        println("Constructor 2 di buat")
+    }
+
+
     var brand = Brand
     var type = Type
     var NoUnit = 0
