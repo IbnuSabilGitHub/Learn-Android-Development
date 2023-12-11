80727cbadf1019c1f4258320f61785f4755b2dc0
IbnuSabil <ibnusabil2301@gmail.com>
2023-12-11 19:51:03 +0800

    Update equals FUnction

--- Learn/Kotlin-OOP/classAndObject/app/equalsFunction.kt
+++ Learn/Kotlin-OOP/classAndObject/app/equalsFunction.kt
@@ -5,26 +5,35 @@ import data.user
 fun main() {
 
     val dataNickName= mutableListOf<user>()
-    print("Masukan Nickname Anda: ")
-    val userNickName = readln()
-    print("Masukan Password Anda: ")
-    val userPassword = readln()
-
     val userObj = user("kitsune","321")
     val userObj2 = user("spg Junior","423")
     val userObj3 = user("nullPlayers","321")
-    val userObjects = user(userNickName, userPassword)
+
     dataNickName += userObj
     dataNickName += userObj2
     dataNickName += userObj3
 
-    when(userObjects){
-        in dataNickName -> readln()
-        !in dataNickName -> dataNickName+= userObjects
-    }
+    var validation = true
+    // Input
+    do {
+        print("Masukan Nickname Anda: ")
+        val userNickName = readln()
+        print("Masukan Password Anda: ")
+        val userPassword = readln()
+        val userObjects = user(userNickName, userPassword) //To object
+        //check apakah nickname sudah di gunakan
+        when(userObjects){
+            in dataNickName -> validation = true // jika ada, set value validation ke true
+            !in dataNickName -> {
+                dataNickName+= userObjects
+                validation = false
+            }
+        }// jika tidak ada maka nickname ini akan di push ke array serta ser value validation ke false
 
-//    println("Array setelah ditambahkan objek:")
+
+    }while (validation)
+
+
     dataNickName.forEach { println(it.nickname) }
-    println(dataNickName[0] == userObjects)
 
 }
