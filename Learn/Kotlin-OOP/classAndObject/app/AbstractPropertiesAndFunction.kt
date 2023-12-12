33d54336526713bab8d05867fd228aee4c8bd9cc
IbnuSabil <ibnusabil2301@gmail.com>
2023-12-12 19:09:49 +0800

    Abstract Properties And Function

---
+++ Learn/Kotlin-OOP/classAndObject/app/AbstractPropertiesAndFunction.kt
@@ -0,0 +1,13 @@
+package Learn.`Kotlin-OOP`.classAndObject.app
+
+import Learn.`Kotlin-OOP`.classAndObject.data.Ayam
+import Learn.`Kotlin-OOP`.classAndObject.data.Iguana
+import Learn.`Kotlin-OOP`.classAndObject.data.animal
+
+fun main() {
+    val iguana = Iguana()
+    iguana.printInfo()
+
+    val chicken = Ayam()
+    chicken.printInfo()
+}
