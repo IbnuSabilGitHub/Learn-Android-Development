b6fc8ae9843347e3afd1bf6c296a3b7a300a67af
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2024-01-11 12:57:57 +0800

    Update Destructuring Declaration

--- Learn/Kotlin-OOP/classAndObject/app/DestructuringDeclaration.kt
+++ Learn/Kotlin-OOP/classAndObject/app/DestructuringDeclaration.kt
@@ -1,11 +1,10 @@
 package Learn.`Kotlin-OOP`.classAndObject.app
 
+import javax.print.attribute.standard.Media
+
 data class user(val name:String, val email:String, val password:String)
 //, val Modus:Int ,val Sorting :Array<Int>
-data class returnMath(val Max:Int, val Min:Int,val median:Int, val Mean:Int, val Sorting:Array<Int>)
-
-
-
+data class returnMath(val Count:Int,val Min:Int, val Max:Int, val Modus:Int, val Median:Double, val Mean:Double, val Sorting:Array<Int>)
 
 fun Array<Int>.bubbleSort() {
     val n = this.size
@@ -19,29 +18,53 @@ fun Array<Int>.bubbleSort() {
         }
     }
 }
+
+
+
 fun mathFunction(data:Array<Int>):returnMath{
 
-    data.sortedArray()//sorting data
+    data.bubbleSort()//sorting data
     var n = data.size//lenght data
     var countArr = 0//jumlah data
-    val median = data[n/2]//Menentukan Median dari data dengan n/2
+
+    val median:Double
+    if(n % 2 != 0){ // jika median ganjil
+        median = data[n/2].toDouble()
+    }else{
+        median = (data[n/2] + data[(n-1)/2]) / 2.0
+    }
+
     var min = data[0]//deklarasi data terkecil
     var max = data[0]//deklarasi data terbesar
+    var frequency = mutableMapOf<Int,Int>() // deklarasi frequency menggunakan mutableMapOf
+    var maxFrequency = 0
+    var modus = -1
+
 
     //forloop untuk mencari value minmax dan jumlah arr
-    for ( i in data){
-        if(i < min){
-            min = i
+    for ( value in data){
+        if(value > min){
+            min = value
         }
-        if (i > max){
-            max = i
+        if (value < max){
+            max = value
         }
-        countArr += i
+        countArr += value
+        frequency[value] = frequency.getOrDefault(value, 0) + 1
     }
-    val mean = countArr/n
+
+    //forloop untuk mendapatkan modu
+    for((key,value) in frequency){
+        if (value > maxFrequency) {
+            maxFrequency = value
+            modus = key
+        }
+    }
+
+    val mean:Double = countArr/n.toDouble()
 
 
-    return returnMath(min,max,median,mean,data)
+    return returnMath(countArr,min,max,modus,median,mean,data)
 }
 
 fun main() {
@@ -52,25 +75,23 @@ fun main() {
 //    println(user.email)
 //    println(user.password)
 
-    val data = arrayOf(1,8,5,3,4,7,2,4,8,5,3,2,1,1,1,6,3,7,3)
+    val data = arrayOf(3, 1, 5, 7, 2, 8, 4, 6)
     val (name,email,password) = user
 
     println(name)
     println(email)
     println(password)
 
-    data.bubbleSort()
-    println(data.toList())
-
-
     val resultData = mathFunction(data)
 
-    val (min,max,median,mean,sorting) = resultData
+    val (count,min,max,modus,median,mean,sorting) = resultData
 
     val result = """
         Jadi hasi data:
+        jumlah   = $count
         minimum  = $min
         maxsimal = $max
+        modus    = $modus
         median   = $median
         mean     = $mean
         sorting  = ${sorting.toList()}
