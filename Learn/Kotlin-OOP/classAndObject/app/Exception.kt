986e8407d4ab8c7ae8c84254038fff2720e5b019
IbnuSabil <ibnusabil2301@gmail.com>
2024-01-12 19:53:21 +0800

    Exception yang bener

--- Learn/Kotlin-OOP/classAndObject/app/Exception.kt
+++ Learn/Kotlin-OOP/classAndObject/app/Exception.kt
@@ -1,2 +1,27 @@
 package Learn.`Kotlin-OOP`.classAndObject.app
 
+data class validationException(val value:String):Throwable(value)
+
+
+fun validationInput(nickname:String) {
+
+    if (nickname.isBlank()){
+        throw validationException("Erro!!, Input anda kosong")
+    } else if( (nickname.length) > 15 ) {
+        throw validationException("Anda melebihi batas input")
+    }
+    println(nickname)
+}
+
+fun main() {
+    try {
+        validationInput("Dodi")
+        validationInput("Dodijefjsdufhksdfiusgedfkjsddsfsdfhsdfuh")
+        validationInput("")
+    } catch (error:validationException){
+        println("erorr ${error.message}")
+        println("erorr ${error.message}")
+    } finally {
+        println("Program berakhir")
+    }
+}
